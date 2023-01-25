457c15ed90b30abdccaa93348dea91ba49551848
NayoonBaek <vlfdyd57@naver.com>
2023-01-25 23:59:46 +0900

    프로그래머스12915: 문자열 내 마음대로 정렬하기

---
+++ Programmers/PGM12915.java
@@ -0,0 +1,32 @@
+package org.example.programmers;
+
+import java.util.Arrays;
+
+//[프로그래머스] 문자열 내 마음대로 정렬하기
+//https://school.programmers.co.kr/learn/courses/30/lessons/12915
+public class PGM12915 {
+    public static String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        for (int i = 0; i < strings.length - 1; i++) {
+            for (int j = i+1; j < strings.length; j++) {
+                if (strings[i].substring(n, n + 1).compareTo(strings[j].substring(n, n + 1)) > 0) {
+                    String tmp = strings[i];
+                    strings[i] = strings[j];
+                    strings[j] = tmp;
+                } else if (strings[i].substring(n, n + 1).compareTo(strings[j].substring(n, n + 1)) == 0) {
+                    if (strings[i].compareTo(strings[j]) > 0) {
+                        String tmp = strings[i];
+                        strings[i] = strings[j];
+                        strings[j] = tmp;
+                    }
+                }
+            }
+        }
+        return answer = strings;
+    }
+
+    public static void main(String[] args) {
+        String[] answer = solution(new String[]{"sun", "bed", "car"}, 1);
+        System.out.println(Arrays.toString(answer));
+    }
+}
