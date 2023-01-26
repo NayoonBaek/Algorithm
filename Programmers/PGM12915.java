cb5db505ef7eff1cb7be71de710a6190b0cab42d
NayoonBaek <vlfdyd57@naver.com>
2023-01-27 02:24:30 +0900

    프로그래머스12915: 문자열 내 마음대로 정렬하기

--- Programmers/PGM12915.java
+++ Programmers/PGM12915.java
@@ -9,11 +9,11 @@ public class PGM12915 {
         String[] answer = {};
         for (int i = 0; i < strings.length - 1; i++) {
             for (int j = i+1; j < strings.length; j++) {
-                if (strings[i].substring(n, n + 1).compareTo(strings[j].substring(n, n + 1)) > 0) {
+                if (strings[i].charAt(n)>(strings[j].charAt(n))) {
                     String tmp = strings[i];
                     strings[i] = strings[j];
                     strings[j] = tmp;
-                } else if (strings[i].substring(n, n + 1).compareTo(strings[j].substring(n, n + 1)) == 0) {
+                } else if (strings[i].charAt(n)==(strings[j].charAt(n))) {
                     if (strings[i].compareTo(strings[j]) > 0) {
                         String tmp = strings[i];
                         strings[i] = strings[j];
